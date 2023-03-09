c6ca9ed4b2128278d387bb93c711c2be4fda1cfb
Manou <manouhaouzi@gmail.com>
2023-03-09 22:29:56 +0200

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,7 @@
 import * as vscode from 'vscode'
-import { extname } from 'path'
-import { getIndent, isFileContainsSemi, isJsFile } from './utils/functions'
 import { disposable1, disposable2, disposable3 } from './utils/disposables'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "console-log-js" is now active!')
   // write console.log
   context.subscriptions.push(disposable1)
   // comment console.log
